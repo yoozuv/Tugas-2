4862aa9c24d54246372f7e7a9a436f08d14a25a1
yoozu <abiyyuv77@gmail.com>
2022-09-14 10:34:23 +0700

    tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_items = CatalogItem.objects.all()
+    context = {
+        'catalog_items': data_catalog_items,
+        'nama': 'Yoozu'
+    }
+    return render(request, "katalog.html",context)
