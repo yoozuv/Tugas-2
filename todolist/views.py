3866b4d46f4da59b23525f4c371d895a858fe6d5
yoozu <abiyyuv77@gmail.com>
2022-10-06 09:55:55 +0700

    Tugas 5

--- todolist/views.py
+++ todolist/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-import datetime
+
 from .models import Task
 from .forms import TaskForm
 from django.http import HttpResponseRedirect
